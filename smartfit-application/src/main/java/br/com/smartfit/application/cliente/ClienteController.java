f89b7d7f026fa08158ef4ca98d3afd8f4bc76ad7
pdcorporation <pd2corporation@gmail.com>
2015-09-09 14:14:56 -0300

    Signed-off-by: pdcorporation <pd2corporation@gmail.com>

--- smartfit-application/src/main/java/br/com/smartfit/application/cliente/ClienteController.java
+++ smartfit-application/src/main/java/br/com/smartfit/application/cliente/ClienteController.java
@@ -1,11 +1,13 @@
 package br.com.smartfit.application.cliente;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import br.com.smartfit.domain.client.bean.Cliente;
 
+@Controller
 @RequestMapping("cliente")
 public class ClienteController {
 
