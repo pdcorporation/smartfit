b0400e7985987b87876cda96f636fd8f139a01c9
pdcorporation <pd2corporation@gmail.com>
2015-09-09 13:10:30 -0300

    Signed-off-by: pdcorporation <pd2corporation@gmail.com>

---
+++ smartfit-web/src/main/java/br/com/smartfit/web/client/SmartFitEntryPoint.java
@@ -0,0 +1,9 @@
+package br.com.smartfit.web.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class SmartFitEntryPoint implements EntryPoint {
+
+  @Override
+  public void onModuleLoad() {}
+}
